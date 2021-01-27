4595ba1b646ca732f93c69211d05006c126ec6ea
Jack Deslippe <jdeslip@gmail.com>
2021-01-26 18:58:42 -0800

    Update motd.js

--- motd.js
+++ motd.js
@@ -1,3 +1,3 @@
 function setMOTD() {
-  document.getElementById("motd").innerHTML = "<font>Best Viewed on Desktop, Laptop or Tablet. Next Update: 2020-01-21 7PM Pacific</font><br><a href='https://docs.google.com/spreadsheets/d/15JSqoXgSOqdYM6D1nuRpRF69Guvpq49TeV5UrrUW0hg/edit?usp=sharing'>Raw Data/Links</a> <a href='https://github.com/jdeslip/stadiastats'>Source Code</a><br><br>";
+  document.getElementById("motd").innerHTML = "<font>Best Viewed on Desktop, Laptop or Tablet. Next Update: 2021-01-28 7PM Pacific</font><br><a href='https://docs.google.com/spreadsheets/d/15JSqoXgSOqdYM6D1nuRpRF69Guvpq49TeV5UrrUW0hg/edit?usp=sharing'>Raw Data/Links</a> <a href='https://github.com/jdeslip/stadiastats'>Source Code</a><br><br>";
 }
