ef22c8df16632a66241af67ef259f27d08020f71
Jack Deslippe <jdeslip@gmail.com>
2021-03-22 20:52:30 -0700

    Update stadiastats.js

--- stadiastats.js
+++ stadiastats.js
@@ -1,6 +1,7 @@
 function setPlot() {
   var myTable = getUrlParameter('table');
   var myName = getUrlParameter('name');
+  var myDiv = getUrlParameter('derivative');
 
   updatePlot(myTable,myName);
 }
@@ -45,12 +46,18 @@ function updatePlot(myTable,myName) {
   $.ajax({url: myURL,dataType: 'json', success: function(result){
     //$("#plot-div").html(result);
     var myArray = new Array();
+    var myDivArray = new Array();
     var myNames = new Array();
 
     for ( var i = 0 ; i < result.length ; i++ ) {
       if (myNames.includes(result[i]['name'])) {
+        var ts_last = new Date(result[i-1]['date']).getTime();
         var ts = new Date(result[i]['date']).getTime();
-        myArray[result[i]['name']].push(new Array(ts,parseInt(result[i]['value'])));
+        var value_last = parseInt(result[i-i]['value']);
+        var value = parseInt(result[i]['value']);
+        myArray[result[i]['name']].push(new Array(ts,value));
+        var derivative = (value-value_last)/parseInt(ts-ts_last);
+        myDivArray[result[i]['name']].push(new Array(ts,derivative));
       } else {
         //console.log(result[i]['name']);
         myNames.push(result[i]['name']);
@@ -63,7 +70,11 @@ function updatePlot(myTable,myName) {
     allSeries=new Array();
    
     for (var i = 0; i < myNames.length; i++) {
-      allSeries.push({ name: myNames[i], data: myArray[myNames[i]], marker:{enabled:true, radius:4}, lineWidth: 4, showCheckbox: false, stickyTracking: false, type: 'scatter'});
+      if (myDiv != "true") { 
+        allSeries.push({ name: myNames[i], data: myArray[myNames[i]], marker:{enabled:true, radius:4}, lineWidth: 4, showCheckbox: false, stickyTracking: false, type: 'scatter'});
+      else {
+        allSeries.push({ name: myNames[i], data: myDivArray[myNames[i]], marker:{enabled:true, radius:4}, lineWidth: 4, showCheckbox: false, stickyTracking: false, type: 'scatter'});
+      }
     }
 
     //console.log(myArray[myNames[0]]);
